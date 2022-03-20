39d57707a58f716d0ef60bd6ea88b899b7b8a8be
João Luiz Ramos <joaoluizramos96@gmail.com>
2022-03-20 15:22:52 -0300

    Eventos no React (onClick onChange e onSubmit) #09

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
 
-  const nome = "Maria"
-
   return (
     <div className="App">
-      <h1>Testando CSS</h1>
-      <Frase />
-      <Frase />
-      <SayMyName nome="João"/>
-      <SayMyName nome="Matheus"/>
-      <SayMyName nome={nome}/>
-      <Pessoa 
-        nome="João"
-        idade="25"
-        profissao="Analista de Redes"
-        foto="https://via.placeholder.com/150"
-      />
-      <List />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1"/>
+      <Evento numero="2" />
+      <Form />
     </div>
   );
 }
