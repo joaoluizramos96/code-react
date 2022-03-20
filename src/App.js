484e3a2fdc06c6bcf1006a3dd90d5c4e02af242a
João Luiz Ramos <joaoluizramos96@gmail.com>
2022-03-20 14:11:40 -0300

    Props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
 
-  const name = "João"
-  const newName = name.toUpperCase();
-
-  function sum(a, b) {
-    return a + b
-  }
-
-  const url = "https://via.placeholder.com/150"
+  const nome = "Maria"
 
   return (
     <div className="App">
-      <h2>Alterando JSX</h2>
-      <p>Olá {newName}!</p>
-      <p>Soma: {sum(2, 2)}</p>
-      <img src={url} alt="Minha imagem" />
+      <SayMyName nome="João"/>
+      <SayMyName nome="Matheus"/>
+      <SayMyName nome={nome}/>
+      <Pessoa 
+        nome="João"
+        idade="25"
+        profissao="Analista de Redes"
+        foto="https://via.placeholder.com/150"
+      />
     </div>
   );
 }
---
+++ src/components/Frase.js
@@ -0,0 +1,11 @@
+function Frase() {
+    
+    return (
+        <div>
+            <h2>Exportando o componente FRASE</h2>
+        </div>
+    )
+
+}
+
+export default Frase
