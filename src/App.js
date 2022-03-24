15bed69b50733f92e02c000dc170658e10e84809
João Luiz Ramos <joaoluizramos96@gmail.com>
2022-03-24 02:10:31 -0300

    Deploy (?)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from './components/layout/Footer';
 function App() {
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <NavBar />
       <Switch>
         <Route exact path='/'>
